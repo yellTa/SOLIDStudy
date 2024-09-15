4bb8531a22b008ffce919cc5e01e7e29c343413e
YellTa <>
2024-09-15 22:07:08 +0900

    AppConfig 리팩토링을 통해서 역할에 따른 구현체목록이 눈에 좀 더 잘보이게 함
    기존에는 중복된 값이 있었는데 이를 줄임으로 역할에 해당하는 구현체만
    변경해주면 된다.
    
    역할이 나오고 역할에 대한 구현이 한 눈에 있다.
    
    큰 장점 : 다른 구현체로 변경할 때 "한 부분"만 변경하면 된다.

--- src/main/java/hello/core/Appconfig.java
+++ src/main/java/hello/core/Appconfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -10,14 +12,22 @@ import hello.core.order.OrderServiceImpl;
 
 //appliction환경 구성은 여기서 다 하는 거임
 public class Appconfig {
-    public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
-    }
+    //역할들이 나오고 역할들이 어떤 구현체를 갖는 지 확인할 수 있다.
+    //역할과 구현 클래스가 한눈에 들어온다.
 
-    public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    public MemberService memberService() {
+        return new MemberServiceImpl(memberRepository());
     }
-
+    public OrderService orderService() {
+        return new OrderServiceImpl(
+                memberRepository(), discountPolicy());
+    }
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
+    }
 
 
 
